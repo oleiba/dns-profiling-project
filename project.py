87e851436967ecd402f9829ccf7753fb9fd52130
Oded Leiba <odedlei@post.bgu.ac.il>
2018-07-21 13:44:30 +0300

    count domain names occurrences using value_counts()

--- project.py
+++ project.py
@@ -32,23 +32,31 @@ import math
 cwd = os.getcwd()
 print(cwd)
 
-DIR = "C:\\Users\\Administrator\\Documents\\MyResearch\\attack-methods\\project"
+DIR = "C:\\Users\\Administrator\\Documents\\MyResearch\\attack-methods\\dns-profiling-project"
 DATA_DIR = 'DNS'
 DOMAIN_SUFFIXES_FILE_NAME = 'tld-desc.csv'
-
 os.chdir(DIR)
 
 with open(DOMAIN_SUFFIXES_FILE_NAME, 'r') as results:
     domainSuffixesReader = csv.reader(results, delimiter=',')
     domainSuffixes = list(domainSuffixesReader)
 
-with open('dnsSummary_user292.pcap.csv', 'r') as results:
-    masqueradeReader = csv.reader(results, delimiter=',')
-    masqueradeRows = list(masqueradeReader)
-
 NUM_OF_USERS = len(os.listdir('.\\' + DATA_DIR))
 print(str(NUM_OF_USERS) + ' users')
-for file in os.listdir('.\\' + DATA_DIR):
+# for file in os.listdir('.\\' + DATA_DIR):
+file = 'dnsSummary_user292.pcap.csv'
+print(file)
+with open(os.path.join(DATA_DIR, file), 'r') as results:
+    df = pd.read_csv(results, delimiter=',')
+    df.head()
+    print(df['dns.qry.name'].values)
+    print(str(len(df['dns.qry.name'].values)) + ' values pre-filter')
     
-
+    # filter to have only IPv4 queries
+    dfOnlyQueries = df[(df['dns.qry.type'] == 1) & (df['dns.flags.response'] == 0)]
+    print(dfOnlyQueries['dns.qry.name'].values)
+    print(str(len(dfOnlyQueries['dns.qry.name'].values)) + ' values post-filter')
+    print(len(dfOnlyQueries['dns.qry.name'].values))
     
+    # count occurrences
+    dfOnlyQueries['dns.qry.name'].value_counts()
